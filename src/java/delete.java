a42b56fd7c89e392b39ee2ccbb30344d3244b218
Justin Ching <justin_ethan_l_ching@dlsu.edu.ph>
2024-11-19 00:48:42 +0800

    delete patient java file

---
+++ src/java/delete.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+import java.util.*;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class delete {
+    public int patient_id;
+    public ArrayList<Integer> patient_idList = new ArrayList<>();
+    
+    public delete(){
+        
+    }
+   
+    public int delete_appointment() {
+        
+        try {
+            // 1. connect with database
+            Connection conn; 
+            conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/school_clinic", "root", "CCINFOM");
+            conn.setAutoCommit(true);
+            
+            // 2. Get the id you need
+            PreparedStatement pstmt;
+            pstmt = conn.prepareStatement("DELETE FROM patients WHERE patient_id = ?");
+            pstmt.setInt(1, patient_id);
+            //pstmt.executeQuery();
+            
+            pstmt.executeUpdate();
+                     
+            // 3. save it
+            pstmt.close();
+            conn.close();
+
+            return 1;
+        } catch (Exception e){
+            System.out.println (e.getMessage());
+            return 0;
+        }
+      
+    }
+    public static void main (String args[]) {
+            delete A = new delete();
+            A.patient_id = 2;
+            A.delete_appointment();
+    }
+}
